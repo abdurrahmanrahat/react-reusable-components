19a40c7085c5dea246530c1b4940cda188551c2c
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2024-01-16 20:26:36 +0600

    modal

--- src/App.tsx
+++ src/App.tsx
@@ -10,13 +10,33 @@ function App() {
     setModal((prev) => !prev);
   };
 
+  // handle submit function
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("clicked");
+
+    if (true) {
+      handleModalClose();
+    }
+  };
+
   return (
     <Container>
       <div className="h-screen w-full flex justify-center items-center">
         <div>
           <Button onClick={() => setModal((prev) => !prev)}>Open Modal</Button>
           <Modal isOpen={modal} onClose={handleModalClose}>
-            <h1>This is Modal</h1>
+            {/* Modal header */}
+            <Modal.Header>
+              <h3>Modal Title</h3>
+              <Modal.CloseButton></Modal.CloseButton>
+            </Modal.Header>
+
+            {/* Modal content */}
+            <form onSubmit={handleSubmit}>
+              <input type="text" />
+              <button type="submit">Submit</button>
+            </form>
           </Modal>
         </div>
       </div>
--- src/components/ui/Modal.tsx
+++ src/components/ui/Modal.tsx
@@ -1,17 +1,101 @@
+import {
+  MouseEvent,
+  ReactNode,
+  createContext,
+  useContext,
+  useRef,
+} from "react";
 import { createPortal } from "react-dom";
 import cn from "../../utils/cn";
 
-const Modal = ({ isOpen, onClose, children }) => {
+const ModalContext = createContext<TModalContext | null>(null);
+
+type TModalContext = {
+  onClose: () => void;
+};
+
+type TModal = {
+  isOpen: boolean;
+  onClose: () => void;
+  children: ReactNode;
+};
+
+type TCloseButton = {
+  children?: ReactNode;
+};
+
+type THeader = TCloseButton;
+
+const Modal = ({ isOpen, onClose, children }: TModal) => {
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  const handleOutsideClose = (e: MouseEvent) => {
+    if (!containerRef.current?.contains(e.target as Node)) {
+      onClose();
+    }
+  };
+
   return createPortal(
-    <div
-      className={cn("fixed inset-0 bg-gray-500/70 invisible z-[999]", {
-        visible: isOpen,
-      })}
-    >
-      <div className="bg-white">{children}</div>
-    </div>,
+    <ModalContext.Provider value={{ onClose }}>
+      <div
+        className={cn(
+          "fixed inset-0 flex items-center justify-center bg-gray-500/70 invisible z-[999]",
+          {
+            visible: isOpen,
+          }
+        )}
+        onClick={handleOutsideClose}
+      >
+        <div
+          ref={containerRef}
+          className="p-4 bg-white w-full max-w-sm rounded-md"
+        >
+          {children}
+        </div>
+      </div>
+    </ModalContext.Provider>,
     document.getElementById("portal") as Element
   );
 };
+
+// close button modal.
+const CloseButton = ({ children }: TCloseButton) => {
+  const { onClose } = useContext(ModalContext) as TModalContext;
+  return (
+    <button className="ml-auto" onClick={onClose}>
+      {children ? (
+        children
+      ) : (
+        <svg
+          className="size-6 bg-[#9330C5] text-white rounded-md p-[2px]"
+          fill="none"
+          strokeWidth="3"
+          stroke="currentColor"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+          aria-hidden="true"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M6 18 18 6M6 6l12 12"
+          ></path>
+        </svg>
+      )}
+    </button>
+  );
+};
+
+// header function
+const Header = ({ children }: THeader) => {
+  return (
+    <div className="flex justify-between items-center w-full mb-3">
+      {children}
+    </div>
+  );
+};
+
+Modal.Header = Header;
+Modal.CloseButton = CloseButton;
 
 export default Modal;
