a68ee9be5f3f534468cda31158b723a81b3094d4
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2024-01-16 17:24:18 +0600

    Reusable Button With Variants

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,13 @@ function App() {
   return (
     <Container>
       <div className="h-screen w-full flex justify-center items-center">
-        <h1>This is a h1 tag</h1>
+        {/* <h1>This is a h1 tag</h1>
         <br />
-        <button className="btn-primary">Submit Button</button> <br />
+        <button className="btn-primary">Submit Button</button> <br /> */}
         <div>
-          <Button outline={true}></Button>
+          <Button variant="outline" className="w-full">
+            Click
+          </Button>
         </div>
       </div>
     </Container>
--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,23 +1,20 @@
 import cn from "../../utils/cn";
 
-// `bg-purple-600 px-3 py-2 rounded-md ${className} ${
-//   variant === "outline" ? "border border-purple-500 bg-opacity-10" : ""
-// }`
+const Button = ({ className, variant }) => {
+  const getVariant = (variant) => {
+    switch (variant) {
+      case "outline":
+        return "btn-outline";
 
-const Button = ({ className, outline }) => {
-  return (
-    <button
-      className={cn(
-        "bg-purple-600 px-3 py-2 rounded-md",
-        {
-          "border border-purple-500 bg-opacity-10": outline,
-        },
-        className
-      )}
-    >
-      Click
-    </button>
-  );
+      case "ghost":
+        return "btn-ghost";
+
+      default:
+        return "btn-solid";
+    }
+  };
+
+  return <button className={cn(getVariant(variant), className)}>Click</button>;
 };
 
 export default Button;
