67a41f6362815b66a977ddb15bfa175612f3455a
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2024-01-16 18:35:30 +0600

    modal config set up

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,23 @@
+import { useState } from "react";
 import Button from "./components/ui/Button";
 import Container from "./components/ui/Container";
+import Modal from "./components/ui/Modal";
 
 function App() {
+  const [modal, setModal] = useState(false);
+
+  const handleModalClose = () => {
+    setModal((prev) => !prev);
+  };
+
   return (
     <Container>
       <div className="h-screen w-full flex justify-center items-center">
-        {/* <h1>This is a h1 tag</h1>
-        <br />
-        <button className="btn-primary">Submit Button</button> <br /> */}
         <div>
-          <Button variant="outline" className="w-full">
-            Click
-          </Button>
+          <Button onClick={() => setModal((prev) => !prev)}>Open Modal</Button>
+          <Modal isOpen={modal} onClose={handleModalClose}>
+            <h1>This is Modal</h1>
+          </Modal>
         </div>
       </div>
     </Container>
