db57b921610f9de4344786906eb670bb08aff39b
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2024-01-17 23:22:10 +0600

    install react hook form

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,43 @@
-import { useState } from "react";
-import Button from "./components/ui/Button";
+import NormalForm from "./components/NormalForm/NormalForm";
 import Container from "./components/ui/Container";
-import Modal from "./components/ui/Modal";
 
 function App() {
-  const [modal, setModal] = useState(false);
+  // const [modal, setModal] = useState(false);
 
-  const handleModalClose = () => {
-    setModal((prev) => !prev);
-  };
+  // const handleModalClose = () => {
+  //   setModal((prev) => !prev);
+  // };
 
-  // handle submit function
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log("clicked");
+  // // handle submit function
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
+  //   console.log("clicked");
 
-    if (true) {
-      handleModalClose();
-    }
-  };
+  //   if (true) {
+  //     handleModalClose();
+  //   }
+  // };
 
   return (
     <Container>
       <div className="h-screen w-full flex justify-center items-center">
-        <div>
+        {/* <div>
           <Button onClick={() => setModal((prev) => !prev)}>Open Modal</Button>
           <Modal isOpen={modal} onClose={handleModalClose}>
-            {/* Modal header */}
-            <Modal.Header>
+
+          <Modal.Header>
               <h3>Modal Title</h3>
               <Modal.CloseButton></Modal.CloseButton>
             </Modal.Header>
 
-            {/* Modal content */}
             <form onSubmit={handleSubmit}>
               <input type="text" />
               <button type="submit">Submit</button>
             </form>
           </Modal>
-        </div>
+        </div> */}
+
+        <NormalForm></NormalForm>
       </div>
     </Container>
   );
