b8209bac94b14e03b7a6990b735db6e3f97db5c0
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2024-01-19 20:56:46 +0600

    animation using keyframe

--- src/pages/Animation/Animation.tsx
+++ src/pages/Animation/Animation.tsx
@@ -3,7 +3,8 @@ import "./Animation.css";
 const Animation = () => {
   return (
     <div className="main">
-      <button className="btn">Hover Me</button>
+      {/* <button className="btn">Hover Me</button> */}
+      <div className="box"></div>
     </div>
   );
 };
