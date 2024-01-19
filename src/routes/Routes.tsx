772ebacc7481a3389468ec03002459a98a88e020
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2024-01-19 21:35:16 +0600

    loading animation

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,13 +1,13 @@
 import { createBrowserRouter } from "react-router-dom";
 import AdminLayout from "../components/layout/AdminLayout";
 import About from "../pages/About/About";
-import Animation from "../pages/Animation/Animation";
 import Home from "../pages/Home/Home";
+import Loading from "../pages/Loading/Loading";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Animation></Animation>,
+    element: <Loading></Loading>,
     children: [
       {
         index: true,
